316edaa42e3ae7f68d054fff6507c7d27a7b109f
gold5472 <gold5471s@gmail.com>
2016-12-17 17:11:14 +0000

    Create dice.js

---
+++ js/dice.js
@@ -0,0 +1,47 @@
+function Dice() // Prepares the dice for rolling
+this.dicespread = 1;
+this.rolled = 0;
+
+}
+
+// Rolling the dice
+Dice.prototype.actuate function self.dice {
+  var Math.random() < 0.125 ? 1 : 2 : 3 : -1 : -2 : -3 : ? : S1;
+  this.rolled = value;
+    
+  }
+  
+
+// Determines what happens when each symbol is rolled
+if (this.rolled = 1); {
+    var this.playervalue = currentvalue + 1; }
+    
+if (this.rolled = 2); {
+    var this.playervalue = currentvalue + 2; }
+  
+if (this.rolled = 3); {
+    var this.playervalue = currentvalue + 3; }
+    
+if (this.rolled = -1); {
+    var this.playervalue = currentvalue - 1; }
+    
+if (this.rolled = -2); {
+    var this.playervalue = currentvalue - 2; }
+    
+if (this.rolled = -3); {
+    var this.playervalue = currentvalue - 3; }
+
+if (this.rolled = ?); {
+    var this.playervalue = currentvalue ± 1,2,3 }
+    } else {
+    var this.playervalue = currentvalue + 1; }
+    var this.otherplayervalue = currentvalue - 1; }
+    
+if (this.rolled = S1); {
+    var this.playervalue = currentvalue + 1; }
+    var this.otherplayervalue = currentvalue - 1; }
+    
+    
+  
+{
+}
