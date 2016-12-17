10e049f92ce0c26fd7df53cd6f33955d9d969f0f
gold5472 <gold5471s@gmail.com>
2016-12-17 17:03:42 +0000

    Create actuator.js

---
+++ js/actuator.js
@@ -0,0 +1,26 @@
+function Actuator() {
+this.action     = document.querySelector(".action");
+this.state      = document.querySelector(".state");
+
+}
+
+Actuator.prototype.actuate = function (Dice, metadata) { // Connects to the dice and metadata
+var self = this;
+
+}
+});
+
+// Stops the game when it is finished
+if (metadata = this.over) { 
+self.data = false;
+} else if (metadata = this.won) {
+self.data = true;
+  
+};
+  
+// Starts the dice
+Actuator.prototype.actuate function self.dice { 
+  var Math.random() < 0.125 ? 1 : 2 : 3 : -1 : -2 : -3 : ? : S1;
+  this.rolled = value;
+    
+  }
