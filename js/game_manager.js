ba894f9980925348ae484018ad3dd6832d35a9b6
gold5472 <gold5471s@gmail.com>
2016-12-17 17:01:40 +0000

    Create game_manager.js

---
+++ js/game_manager.js
@@ -0,0 +1,29 @@
+function GameManager(sizeofcards, Actuator, Dice)
+this.launchgame = true // Start the game
+this.sizeofcards = 28% // Size of the 2s
+this.won = false
+this.over = false
+this.state = state.normal
+this.action = true // Makes game function
+this.playervalue === 0
+this.otherplayervalue === 0
+self.data = true
+
+}
+};
+
+// Clear the state when the game is over
+if (this.over = true) {
+this.clear = true;
+} else {
+this.state = state.normal
+
+}
+};
+
+if (this.clear = true) return; // Don't do anything if the game is over
+
+// The Winner
+if (this.playervalue === 15) this.won = true;
+var (this.action = false)
+
